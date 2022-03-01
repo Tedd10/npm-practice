c9eb1d973ec84846a9ca239c6fb39c81a1e3bfa0
Theodore Akpati <theodoreakapati@gmail.com>
2022-03-01 01:06:59 -0600

    pluralize npm files

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-document.getElementById("main").textContent = 
-  dayjs().format("dddd D MMMM YYYY");
+const mainEl = document.getElementById("main");
+
+mainEl.textContent = dayjs().format("dddd D MMMM YYYY");
+
+mainEl.insertAdjacentHTML (
+  "afterend",
+  `<h2>${validator.isEmail('foo@bar.com')}</h2>`
+);
